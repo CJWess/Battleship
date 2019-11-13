f8555f798c7b8a1161f8a10e0972e786fb1ace52
ChaseWessman <chase.wessman@regalcuttingtools.com>
2019-11-13 12:29:32 -0600

    Add next steps comments to Board

---
+++ Battleship/Board.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public class Board
+    {
+        public int Width { get; }
+        public int Height { get; }
+
+        private List<Ship> ships;
+        public Board(int width, int height, params int[] shipLengths)
+        {
+            Width = width;
+            Height = height;
+            InitShips(shipLengths);
+        }
+
+        // will have some collection of ships
+        private void InitShips(int[] shipLengths)
+        {
+            ships = new List<Ship>();
+
+            //generate random start point
+            //ship has to be within board bounds
+            //ship can't overlap any other ships already existing (run Overlaps against every previous ship)
+            //(maybe set a number of tries ~a few thousand or so) if they all fail, move on
+
+        }
+    }
+}
